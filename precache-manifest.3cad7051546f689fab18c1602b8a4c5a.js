c376052bda2f277545dd457ac046ca6baeddaddd
yadvi12 <yadvibhalla2002@gmail.com>
2020-05-03 19:37:20 +0530

    Updates

---
+++ precache-manifest.3cad7051546f689fab18c1602b8a4c5a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131e25c0209093deb8d36e4f79cf15e0",
+    "url": "/Keep/index.html"
+  },
+  {
+    "revision": "b654a98c23e251566cb5",
+    "url": "/Keep/static/js/2.574e01f9.chunk.js"
+  },
+  {
+    "revision": "2cf537240edbe32271a0",
+    "url": "/Keep/static/js/main.71fb72b2.chunk.js"
+  },
+  {
+    "revision": "2d2f085acaaa86b9fd04",
+    "url": "/Keep/static/js/runtime-main.8189dec5.js"
+  }
+]);
